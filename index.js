079939229978fa54510fa2d5b22f51fa2f678bee
autmanuel <manuel.hirschbeck@gmail.com>
2023-12-12 19:07:59 +0100

    changes button behaviour

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 
 window.onload = () => {
+    let dateShown = false;
     console.log('Hello World');
 
     const buttonElement = document.getElementById('button');
@@ -14,7 +15,15 @@ window.onload = () => {
         if(!displayElement)
             throw new Error(`Display Element not found!!`);
 
-        displayElement.innerHTML = Date();
+        // date shown is default false!!
+
+        if(!dateShown) { // when datesshown is false
+            displayElement.innerHTML = Date(); // display date text
+            dateShown = true; // set date shown to true
+        } else { // if dateshown is true 
+            displayElement.innerHTML = ''; // reset display to empty
+            dateShown = false; // set date shown to false;
+        }
     }
 
     buttonElement.addEventListener('click',eventListener);
